3479b68fe796931e13349cc020bd001d29bdeb10
George <warkung@gmail.com>
2025-06-14 19:44:35 +0700

    Install Codemod lagasy img

--- components/card/LandmarkCard.tsx
+++ components/card/LandmarkCard.tsx
@@ -12,19 +12,28 @@ export default function LandmarkCard({
 
   return (
     <article className="group relative items-center">
-      <div className="relative h-[300px] ">
+      <div className="relative h-[150px] ">
         <Image
           src={image}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           alt={name}
-          fill
-          className="rounded-lg group-hover:scale-105 transition-all duration-300"
+          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          // fill
+          sizes="300"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg group-hover:scale-105 transition-all duration-300 max-h-[150px]"
+          priority
+          placeholder="blur"
+          blurDataURL={image}
         />
       </div>
       <div className="mt-4 flex justify-between">
         <h3 className="text-lg font-semibold">{name}</h3>
         <LandmarkRating />
       </div>
+      <p className="text-sm text-muted-foreground">
+        {description.substring(0, 40)}
+      </p>
     </article>
   );
 }
